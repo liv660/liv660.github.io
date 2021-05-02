6af02ab46e00f501cf8eafcd578b40d8f3639bd3
Olivia <liv660n@gmail.com>
2021-05-02 23:11:16 +0900

    Update SemiProject

--- assets/js/_main.js
+++ assets/js/_main.js
@@ -134,12 +134,4 @@ $(document).ready(function() {
     }
   });
 
-  // $('.fade').slick({
-  // dots: true,
-  // infinite: true,
-  // speed: 500,
-  // fade: true,
-  // cssEase: 'linear'
-  // });
-
 });
